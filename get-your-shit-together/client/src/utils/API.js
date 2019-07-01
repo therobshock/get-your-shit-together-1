5864ca0f7f110c7e3d64cb4f9f650c5d9cbc4a8f
Robert Bolton <therobshock@yahoo.com>
2019-07-01 16:18:55 -0700

    some tests

--- get-your-shit-together/client/src/utils/API.js
+++ get-your-shit-together/client/src/utils/API.js
@@ -1,36 +1,36 @@
-import axios from "axios";
+import axios from "axios"; 
 
 export default {
   // Gets all shit
   getShit: function() {
-    return axios.get("/api/shits");
+    return axios.get("http://localhost:3001/api/shit");
   },
   // Gets the shit with the given id
   getOneShit: function(id) {
-    return axios.get("/api/shits/" + id);
+    return axios.get("http://localhost:3001/api/shit" + id);
   },
   // Deletes the shit with the given id
   deleteShit: function(id) {
-    return axios.delete("/api/shits/" + id);
+    return axios.delete("http://localhost:3001/api/shit" + id);
   },
   // Saves a shit to the database
   saveShit: function(shitData) {
-    return axios.post("http://localhost:3001/api/shits", shitData);
+    return axios.post("http://localhost:3001/api/shit", shitData);
   },
   // Gets all users
   getUsers: function() {
-    return axios.get("/api/users");
+    return axios.get("http://localhost:3001/api/user");
   },
   // Gets the user with the given id
   getUser: function(id) {
-    return axios.get("/api/users/" + id);
+    return axios.get("http://localhost:3001/api/user" + id);
   },
   // Deletes the user with the given id
   deleteUser: function(id) {
-    return axios.delete("/api/users/" + id);
+    return axios.delete("http://localhost:3001/api/user" + id);
   },
   // Saves a user to the database
   saveUser: function(userData) {
-    return axios.post("/api/users", userData);
+    return axios.post("http://localhost:3001/api/user", userData);
   },
 };
